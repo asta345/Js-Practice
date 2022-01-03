d85980acc1b63cbe910204b66ede4ed840b0aa26
asta345 <81383112+asta345@users.noreply.github.com>
2022-01-03 23:31:26 +0530

    Update and rename basic solving to basic solving.js

---
+++ basic solving.js
@@ -0,0 +1,46 @@
+Find area of rectangle?
+var length = prompt("Enter a whole number for the length of your rectangle.");
+var width = prompt ("Enter a whole number for the width of your rectangle.");
+
+function area (length, width) {
+return length * width;
+}
+
+console.log(The area of your rectangle is " + area);
+
+find the assic value of character?
+// take input from the user
+const string = prompt('Enter a character: ');
+
+// convert into ASCII value
+const result = string.codePointAt(0);
+
+console.log(`The ASCII value is: ${result}`);
+
+// program to convert celsius to fahrenheit
+// ask the celsius value to the user 
+const celsius = prompt("Enter a celsius value: ");
+
+// calculate fahrenheit
+const fahrenheit = (celsius * 1.8) + 32
+
+// display the result
+console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
+
+
+//take input from the users
+let a = prompt('Enter the first variable: ');
+let b = prompt('Enter the second variable: ');
+
+//create a temporary variable
+let temp;
+
+//swap variables
+temp = a;
+a = b;
+b = temp;
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
+
+
